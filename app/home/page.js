f8b1f1b6c8b1d4f660beb3c6692eeac60af5d66e
Olanrewaju Emmanuel <olanrewajuemma2023@gmail.com>
2026-03-28 14:49:03 +0100

    removed debug satetement

--- app/home/page.js
+++ app/home/page.js
@@ -9,7 +9,6 @@ export default function HomePage() {
   const { posts, hasMore, fetchPosts, loadMore } = usePostsStore()
 
   useEffect(() => {
-    console.log('HOME PAGE LOADED')
     fetchPosts(1)
   }, [fetchPosts])
 
